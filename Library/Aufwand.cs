97bb8a040347e42e935b9e8735e7ee74f96de96d
Martin Aischmann <martin.aischmann@gmail.com>
2022-06-11 15:04:00 +0200

    Gui 95%+Sqlite Listen Erträge und Aufwände

---
+++ Library/Aufwand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Aufwände
+    {
+        public int ID { get; set; }
+        public string Aufwand { get; set; }
+        public decimal Wert { get; set; }
+    }
+}
